456149bf29cec368e9b8ad2ff4383bfb4fef99b8
Muxutdinov <jmuxutdinov@mail.ru>
2022-02-05 15:26:41 +0500

    add forms

--- src/components/AddForms.jsx
+++ src/components/AddForms.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const AddForms = () => {
-  return <div>AddForms</div>;
+  return <form className='border p-4 shadow-lg rounded'>
+    <div className='mb-3'>
+       <label className='form-label fs-4'>Title for new news?</label>
+       <input type='text' required name='name' className='form-control' id="name" placeholder='Write title news' />
+    </div>
+    <div className='mb-3'>
+       <label className='form-label fs-4'>Text</label>
+       <textarea type='text' required name='name' className='form-control' id="name"  placeholder='Text' />
+    </div>
+    <div className='mb-3'>
+       <label htmlFor="category" className='from-label'>Choose Category of news</label>
+       <select required className='form-select' id='category' name='category'> 
+         <option>Choose category of news</option>
+         <option value='Hot'>Hot</option>
+         <option value='world'>world</option>
+         <option value='Sport'>Sport</option>
+         <option value='football'>football</option>
+       </select>
+    </div>
+    <button type='submit' className='btn-dark btn shadow-lg'>Add new news</button>
+  </form>;
 };
 
 export default AddForms;
--- src/components/FilterContent.jsx
+++ src/components/FilterContent.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const FilterContent = () => {
-  return <div>FilterContent</div>;
+  return <div className='card shadow-lg mt-4'>
+      <div className='card-body'>
+        <p className='card-text'>Filter by category</p>
+        <div className='btn-group'>
+          <button className='btn btn-outline-dark active'>All</button>
+          <button className='btn btn-danger'>Hot News</button>
+          <button className='btn btn-secondary'>Sport News</button>
+          <button className='btn btn-primary'>World News</button>
+          <button className='btn btn-warning'>Football News</button>
+        </div>
+      </div>
+  </div>;
 };
 
 export default FilterContent;
