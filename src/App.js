8417e0f805ec5af3551372bed7e68548b016f1cf
Muxutdinov <jmuxutdinov@mail.ru>
2022-02-07 16:32:45 +0500

    Update redux-project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import './index.scss'
-import Navbar from './components/Navbar'
-import NewsList from './components/NewsList';
-import AddForms from './components/AddForms';
-import FilterContent from './components/FilterContent';
+import { useSelector } from 'react-redux';
+import './App.css';
+import Likes from './Likes';
+import Title from './Title';
+import Comments from './Comments';
+import Spin from './Spin';
 
+function App() {
+  const error = useSelector(state => state.appReducer.error);
 
-
-
-const App = () => {
-  return <div className='app'>
-    <Navbar/>
-    <div className='content'>
-      <NewsList/>
-      <div className='content_page'>
-        <AddForms/>
-        <FilterContent/>
+  console.log('error >>> ', error)
+  return (
+    <div className="App">
+      <div className="wrap">
+        <Spin />
+        <div className="card">
+          {error && (
+            <div className='error-message'>
+              {error}
+            </div>
+          )}
+          <div className="card-image">
+            <img src="https://smartcity.press/wp-content/uploads/2017/11/smart-and-sustainable-paris.jpg" alt="surfing"/>
+            <Title />
+            <Likes />
+          </div>
+          <Comments />
+        </div>
       </div>
     </div>
-  </div>;
-};
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { rootReducer } from './redux/rootReducer';
+import { Provider } from 'react-redux';
+import { spamFilter } from './redux/middleware';
+import './index.css';
 import App from './App';
-import './index.scss';
-import {Provider} from 'react-redux'
-import {createStore} from 'redux'
-import reducer from './redux/reducer'
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk,
+    spamFilter
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 ReactDOM.render(
-  <Provider store={store}>
-  <App />,
-  </Provider>,
+<Provider store={store}>
+  <App />
+</Provider>,
   document.getElementById('root')
 );
+
